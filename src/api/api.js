33e6a9af71a84b2c72288179db8c9eefbc5a1801
stanislav_s <stanislav_s@maximail.org>
2022-10-28 01:48:14 +0500

    Removed axios from api parameters

--- src/api/api.js
+++ src/api/api.js
@@ -4,32 +4,29 @@ const $axios = axios.create({
   baseURL: "http://localhost:7777",
 });
 
-async function getUsers(axios = $axios) {
-  return await axios.get("/");
+async function getUsers() {
+  return await $axios.get("/");
 }
 
-async function getMessages(data, axios = $axios) {
-  return await axios.post("/getmessagestime", data);
+async function getMessages(data) {
+  return await $axios.post("/getmessagestime", data);
 }
 
-async function addUser(data, axios = $axios) {
-  return await axios.post("/addUser", data);
+async function addUser(data) {
+  return await $axios.post("/addUser", data);
 }
 
-async function addMessage(data, axios = $axios) {
-  return await axios.post("/addMessage", data);
+async function addMessage(data) {
+  return await $axios.post("/addMessage", data);
 }
 
-async function getFriends(data, axios = $axios) {
-  return await axios.post("/getFriends", data);
+async function getFriends(data) {
+  return await $axios.post("/getFriends", data);
 }
 
-async function getFriendsCount(data, axios = $axios) {
-  return await axios.post("/countFriends", data);
+async function getFriendsCount(data) {
+  return await $axios.post("/countFriends", data);
 }
-/*async function saveTodos(axios = $axios, data) {
-  return await axios.post("/save", data);
-}*/
 
 export {
   getUsers,
